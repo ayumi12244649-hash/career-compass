27be003f1239536b410c61d3df10e86729559385
Ayumi Okita <ayumi12244649@gmail.com>
2026-07-12 19:15:29 +0900

    Disable SW fetch cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "career-compass-v4";
+const CACHE_NAME = "career-compass-v5";;
 
 const urlsToCache = [
   "/",
@@ -34,26 +34,3 @@ self.addEventListener("activate", (event) => {
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) {
-        return cached;
-      }
-
-      return fetch(event.request)
-        .then((response) => {
-          const responseClone = response.clone();
-
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, responseClone);
-          });
-
-          return response;
-        })
-        .catch(() => caches.match("/"));
-    })
-  );
-});
